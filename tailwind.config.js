28e36ab66287a9b0188de0ecaa004f147a77271a
Deflotion <danny27revaldo@gmail.com>
2023-09-13 23:23:48 +0700

    commit untuk pertama kali dan pembuatan about me duluan

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./public/index.html'],
+  theme: {
+    extend: {
+      letterSpacing: {
+        palinglebar : '.5em',
+      }
+    },
+  },
+  plugins: [],
+}
+
